257f4ca4b31588a7ec3bac24425130977ea7c04f
Gawasingha Arachchige Chamindu Kavya <162815316+Chamindukavya@users.noreply.github.com>
2025-02-02 20:00:33 +0530

    maze done

--- maze.py
+++ maze.py
@@ -13,7 +13,9 @@ class StackFrontier():
         self.nodeList.append(node)
         
     def remove(self):
+        node = self.nodeList[-1]
         del self.nodeList[-1]
+        return node
 
     def empty(self):
         if len(self.nodeList) == 0:
@@ -21,23 +23,28 @@ class StackFrontier():
         else:
             return False  
 
-
+    def checkForState(self,state):
+        for i in self.nodeList:
+            if i.state == state:
+                return True
+                 
+        return False
 class Maze():
     
     def __init__(self,fileName):
         
         with open(fileName,'r') as f:
-            maze = f.read()
-            maze = maze.splitlines()
-            print(maze)
+            self.maze1 = f.read()
+            maze = self.maze1.splitlines()
+            # print(maze)
 
-            height = len(maze)
-            width = len(maze[0])
+            self.height = len(maze)
+            self.width = len(maze[0])
 
-            walls = []
-            for i in range(height):
+            self.walls = []
+            for i in range(self.height):
                 rawWall = []
-                for j in range(width):
+                for j in range(self.width):
                     
                     if maze[i][j] == 'A':
                         self.startPoint = (i,j)
@@ -49,12 +56,99 @@ class Maze():
                         rawWall.append(False)
                     else:
                         rawWall.append(True)
-                walls.append(rawWall)
+                self.walls.append(rawWall)
 
-            frontier = StackFrontier()    
-            startPoint = Node(self.startPoint,None)
-            frontier.add(node=startPoint)    
+              
+    def neighbors(self,state):
+        (raw,colum) = state
+
+        possibleActions = [
+            ("up",raw-1,colum),
+            ("down",raw+1,colum),
+            ("left",raw,colum-1),
+            ("right",raw,colum+1)
+        ]
+
+        actionsList = []
+        for action,r,c in possibleActions:
+            if (0<= r <self.height) and (0<= c <self.width):
+                if not self.walls[r][c]:
+                    actionsList.append((r,c))
+        return actionsList
+
+    def solve(self):
+        
+        visitedNodes = []
+        frontier = StackFrontier()    
+        startPoint = Node(self.startPoint,None)
+        frontier.add(node=startPoint)
+
+        while True:
+            if frontier.empty():
+                return 
+            
+            nodeRemoveFromFrontier = frontier.remove()
+            visitedNodes.append(nodeRemoveFromFrontier.state)
+
+            possibleActions = self.neighbors(nodeRemoveFromFrontier.state)
+
+            for i in possibleActions:
+                # print("first" ,i)
+                # print("visited nodes",frontier.checkForState(i))
+                if i not in visitedNodes and (not frontier.checkForState(i)):
+                    # print("I are",i)
+                    if (i == self.goal):
+                        print("goal is",self.goal)
+                        self.print(nodeRemoveFromFrontier)
+                        return
+                    newNode = Node(i,nodeRemoveFromFrontier)  #given the node as the parent
+                    frontier.add(newNode)
+
+    def print(self,node):
+        # print(self.maze1)
+        # print(node.state)
+
+        path = []
+        path.append(node.state)
+        parent = node.parent
+        while parent != None:
+            path.append(parent.state)
+            parent = parent.parent
+
+        # print(path)    
+
+        maze2 = self.maze1.splitlines()
+        c = -1
+        r = -1
+        for i in maze2:
+            c += 1
+
+            for j in i:
+                r += 1
+                # print(c,r)
+                if j == '#':
+                    print("█", end="")
+                elif j=='A':
+                    print("A",end="")    
+                elif j=='B':
+                    print("B",end="")
+                elif j==" " and (c,r) in path:
+                    print("*",end="")
+
+                else:
+                    print(" ",end="")  
+            print("/n")
+            r = -1                  
+
+        print((7,13) in path)
+
+        
+        
+
+
+
           
               
         
-m = Maze(sys.argv[1])        
+m = Maze(sys.argv[1])  
+m.solve()      
